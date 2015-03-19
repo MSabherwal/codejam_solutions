a3ff08ca2ea4d66f0d888c336bfe4ab15169a385
Manu Sabherwal <msabherw@gmail.com>
2015-03-19 13:00:55 -0700

    finished Alien_language

--- Alien_Language/main.rb
+++ Alien_Language/main.rb
@@ -21,67 +21,6 @@ if clo[:input].nil? or clo[:output].nil?
 	raise('require both input and output files')
 end
 
-def valid_combinations(line:,word_len:,valid_words:)
-
-	char_index = 0
-	line_seg = ''
-	words = Set.new()
-	while char_index < word_len
-		
-		if line[0] == '('
-			letters = []
-			line.slice!(0) # get rid of '('
-			while line[0] != ')' do
-				char = line.slice!(0)
-				
-				letters << char		
-			end
-			 
-			#if letters is empty, there are no valid letters at that index
-			# therefore, there are no matches
-			if letters.empty?
-				return []
-			end
-			
-			words_cp = Array.new(letters.length) {Set.new(words)}
-			# puts "words_cp = #{words_cp}"
-			letters.each_index do |letter_index|
-				# puts "letter at index #{letter_index} = #{letters[letter_index]}"
-				if words_cp[letter_index].empty?
-					words_cp[letter_index].add(letters[letter_index])
-				else
-					words_cp[letter_index].map! do |x|
-						x+=letters[letter_index] 
-					end
-				end
-				# puts words_cp.inspect
-			end
-
-			#merge all words to one array
-			words_cp.each do |x|
-				words = words.union(x)
-			end
-			# puts "words = #{words.inspect}"
-
-			line.slice!(0) # remove the ')'
-			
-			return words if line.empty?
-		else #static characters
-			if words.empty?
-				words.add(line.slice!(0))
-			else
-				words.map do |x|
-					x+=line[0]
-				end
-				line.slice!(0)
-			end
-		end
-		char_index +=1
-	end
-	puts "words = #{words.inspect}"
-	return valid_words.intersection(words)
-end
-
 
 input = File.open(clo[:input],'r')
 
@@ -89,23 +28,30 @@ output = File.open(clo[:output], 'w')
 
 word_len,num_words,num_cases = input.readline.split(" ").map do |x| x.to_i end
 
-valid_words = Set.new()
-
+words = []
 
 (1..num_words).each do |word_num|
 	word = input.readline.strip!
-	valid_words.add(word)
+	words << word
 
 end
-puts "valid_words = #{valid_words.inspect}"
+
+def word_to_regex(pattern:)
+	return Regexp.new(pattern.gsub(/[ \(\) ]/,'(' => '[',')' => ']'))
+
+end
 
 
 (1..num_cases).each do |case_num|
 	line = input.readline.strip!
+
+	reg_ex = word_to_regex(pattern: line)
 	
-	valid_comb = valid_combinations(line: line,word_len: word_len,valid_words: valid_words)
-	puts "valid_comb = #{valid_comb.inspect}"
-	puts "valid_words = #{valid_words.inspect}"
+	count = 0
+	words.each do |word|
+		count +=1 if word =~ reg_ex
+	end
+	output.write("Case \##{case_num}: #{count}\n")
 	
 end
 
